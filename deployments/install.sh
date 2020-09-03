4f9f6faac65c6653318bb7223e7e9cb33f26790e
Oleksandr Semianikhin <Oleksandr_Semianikhin@epam.com>
2020-09-03 17:49:13 +0300

    startup script

--- deployments/install.sh
+++ deployments/install.sh
@@ -5,8 +5,10 @@ sudo yum -y install yum-utils device-mapper-persistent-data lvm2
 sudo yum -y remove docker-client docker-common docker
 
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+yum install -y https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm
 	
-sudo yum -y install docker-ce
+sudo yum -y install docker-ce docker-ce-cli
 
 sudo systemctl start docker
 sudo systemctl enable docker
