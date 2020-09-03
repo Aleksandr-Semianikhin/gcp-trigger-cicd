73da84e1164351d67ab325e521e8e7787ba937a4
Oleksandr Semianikhin <Oleksandr_Semianikhin@epam.com>
2020-09-03 15:56:41 +0300

    Change startup script

--- deployments/install.sh
+++ deployments/install.sh
@@ -1,11 +1,15 @@
-sudo yum install -y yum-utils
+sudo yum -y update
 
-sudo yum-config-manager \
-    --add-repo \
-    https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum -y install yum-utils device-mapper-persistent-data lvm2
+
+sudo yum -y remove docker-client docker-common docker
+
+sudo yum-config-manager --add-repo 
+https://download.docker.com/linux/centos/docker-ce.repo
 	
-sudo yum install docker-ce docker-ce-cli containerd.io
+sudo yum -y install docker-ce
 
 sudo systemctl start docker
+sudo systemctl enable docker
 
-docker run -d -p 8081:8081 --name nexus sonatype/nexus3	
+sudo docker run -d -p 8081:8081 --name nexus sonatype/nexus3	
